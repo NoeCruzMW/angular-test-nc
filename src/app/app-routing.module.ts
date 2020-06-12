455c81afece3a4dd77eea2847bdb9d4983559c8f
Noe Cruz | Zurck'z <noe.isc20@gmail.com>
2020-06-12 13:05:54 -0500

    :package: Packages and basic project settings added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { EmployeesComponent } from './components/employees/employees.component';
+import { GroupsComponent } from './components/groups/groups.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', component: HomeComponent
+  },
+  {
+    path: 'employees', component: EmployeesComponent
+  },
+  {
+    path: 'groups', component: GroupsComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,23 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CarouselModule } from 'ngx-bootstrap/carousel';
+import { HomeComponent } from './components/home/home.component';
+import { EmployeesComponent } from './components/employees/employees.component';
+import { GroupsComponent } from './components/groups/groups.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    EmployeesComponent,
+    GroupsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CarouselModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
